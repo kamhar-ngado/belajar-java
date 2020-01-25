fbba766097dcfa76abb0e99bffb657b69c13d92c
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-26 02:14:44 +0700

    add TipeData

---
+++ TipeData.java
@@ -0,0 +1,30 @@
+public class TipeData
+{
+    public static void main(String[] args)
+    {
+        byte digit = 100;
+        short harga = 900;
+        int tahun = 2020;
+        long jarak = 100000L;
+        float suhu = 26.12f;
+        Double gaji = 1000.0;
+        Boolean jomblo = true;
+        String tampan = "iya";
+
+        System.out.println(digit);
+        System.out.println(harga);
+        System.out.println(tahun);
+        System.out.println(jarak);
+        System.out.println(suhu);
+        System.out.println(gaji);
+        System.out.println(jomblo);
+        System.out.println(tampan);
+
+
+
+
+
+
+
+    }
+}
