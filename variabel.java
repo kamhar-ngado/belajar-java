41af487abdef11328060b11d24cc2802bc0c6a28
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-25 13:50:51 +0700

    varibel

---
+++ variabel.java
@@ -0,0 +1,25 @@
+public class variabel
+{
+    public static void main (String[] args)
+    {
+        String nama_depan = "kamhar ";
+        String nama_belakang = "ngado";
+        String nama_lengkap = nama_depan + nama_belakang;
+        int tahun = 2020;
+
+        Double IPK;
+        IPK = 4.0;
+
+        int x = 2;
+        int y = 3;
+        int z = 4;
+        int hasil_perhitungan = x + y -z;
+
+        System.out.println("x=2, y=3 dan z=4");
+        System.out.println("hasil dari x+y-z=" + hasil_perhitungan);
+        System.out.println("nama saya adalah " + nama_lengkap);
+        System.out.println("IPK saya adalah " + IPK);
+        System.out.println("sekarang tahun " + tahun);
+
+    }
+}
