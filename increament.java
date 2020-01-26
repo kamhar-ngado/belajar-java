c9b874568b3d5b5a3d0d93e812298a6dc2d43851
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-26 13:59:35 +0700

    add increament

---
+++ increament.java
@@ -0,0 +1,13 @@
+public class increament
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        a++; //a++ or a=a+1
+        int b= 4;
+        b--; // b-- or b=b-1
+
+        System.out.println("nilai a akan bertambah 1 sehingga a=" + a);
+        System.out.println("nilai b akan berkurang 1 sehingga a=" + b);
+    }
+}
--- operator.java
+++ operator.java
@@ -1,6 +1,6 @@
 public class operator
 {
-    public static void main(String[] argss)
+    public static void main(String[] args)
     {
         int a = 2;
         int b = 4;
