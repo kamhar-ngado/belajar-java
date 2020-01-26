90ec16662db7749a53cd3b7098118c134c5921ce
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-26 13:46:56 +0700

    add operator

--- operator.java
+++ operator.java
@@ -2,11 +2,18 @@ public class operator
 {
     public static void main(String[] argss)
     {
-        int a = 1;
-        int b = 2;
+        int a = 2;
+        int b = 4;
         int c = a+b;
+        int d = b-a;
+        int e = c/a;
+        int f = e*c;
+        int g = (f % b);
 
-        System.out.println("diketahui a= 1, b=2, c=a+b");
-        System.out.println("hasil penjumlahan dari c= " + c);
+        System.out.println("hasil dari d=" + d);
+        System.out.println("hasil dari c=" + c);
+        System.out.println("hasil dari e=" + e);
+        System.out.println("hasil dari f=" + f);
+        System.out.println("hasil dari g=" + g);
     }
 }
