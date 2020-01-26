c6384b2291c1b1c04ccd1e571f1dda9ff2ea3487
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-26 13:22:20 +0700

    operasi penjumlahan

---
+++ operator.java
@@ -0,0 +1,12 @@
+public class operator
+{
+    public static void main(String[] argss)
+    {
+        int a = 1;
+        int b = 2;
+        int c = a+b;
+
+        System.out.println("diketahui a= 1, b=2, c=a+b");
+        System.out.println("hasil penjumlahan dari c= " + c);
+    }
+}
