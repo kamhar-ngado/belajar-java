7b6c310a7e5da2cfe09674c2d037cf423b5b9971
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-01-28 20:09:43 +0700

    add perbandingan

---
+++ perbandingan.java
@@ -0,0 +1,17 @@
+public class perbandingan 
+{
+    public static void main(String[] args)
+    {
+        int a = 1;
+        int b = 2;
+        int c = 1;
+
+        System.out.println(a!=b);
+        System.out.println(a==c);
+        System.out.println(b<1);
+        System.out.println(b>1);
+
+
+        
+    }
+}
