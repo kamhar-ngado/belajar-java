07de77dc0bac76215508b5be6a30058fe0d48f58
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-02-04 15:13:55 +0700

    edit

--- method.java
+++ method.java
@@ -1,13 +1,19 @@
-public class method
-{
-    static void kamhar()
-    {
-        System.out.println("kamhar is handsome");
+public class method {
+    static void kamhar() {
+        boolean handsome = true;
+        if (handsome = false) {
+            System.out.println("kamu tidak handsome (not " + handsome + " handsome)");
+        } else {
+            System.out.println("cuma kamhar yang paling handsome");
+        }
+        String[] handsoem = { "yea.. handsoem", "al right you are handsoem", "no one handsoem than you kamhar" };
+        for (String i : handsoem) {
+            System.out.println(i);
+        }
     }
 
-public static void main(String[] args)
-{
-    kamhar();
-}
+    public static void main(String[] args) {
+        kamhar();
+    }
 
 }
