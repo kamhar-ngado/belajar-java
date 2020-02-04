52393d4276432ac0afc825eab8fd9c5e2a5862fd
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-02-04 14:35:50 +0700

    add method

---
+++ method.java
@@ -0,0 +1,13 @@
+public class method
+{
+    static void kamhar()
+    {
+        System.out.println("kamhar is handsome");
+    }
+
+public static void main(String[] args)
+{
+    kamhar();
+}
+
+}
