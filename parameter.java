cc429a576ff10a3a877f0a3882d674a9c5cd2a36
kamhar-ngado <52423991+kamhar-ngado@users.noreply.github.com>
2020-02-06 20:58:17 +0700

    add parameter

---
+++ parameter.java
@@ -0,0 +1,13 @@
+public class parameter
+
+{
+    static void nama(String tamvan) {
+        System.out.println(tamvan + "handsome");
+    }
+
+    public static void main(String[] args) {
+        nama("kamhar ");
+        nama("kamhar selalu ");
+        nama("memang ");
+    }
+}
